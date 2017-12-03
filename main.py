9278c58dbb5cc3de703031b4195cd825af0fd8e0
majojos <33401919+majojos@users.noreply.github.com>
2017-12-03 11:58:38 +0100

    comments added

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+Example usage of module timing
+"""
+
 from timing import Timing
 import time
 
@@ -13,3 +17,13 @@ my_inner_process.stop()
 
 time.sleep(1)
 my_outer_process.stop()
+
+#Output:
+# 'My outer process' started.
+# 'My inner process' started.
+# 'My inner process' finished.
+# ++++++++++++SUMMARY+EXECUTION+TIME+'My inner process'+++++++++++++
+# Elapsed time 'My inner process':0 min, 2.000 sec
+# 'My outer process' finished.
+# ++++++++++++SUMMARY+EXECUTION+TIME+'My outer process'+++++++++++++
+# Elapsed time 'My outer process':0 min, 5.000 sec
